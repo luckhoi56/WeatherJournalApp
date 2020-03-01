72719e42b2ad75ed6f1a97a46b0637a8a49bc7e1
Eric Luc <ericluc@Erics-MacBook-Pro.local>
2020-02-29 21:18:35 -0500

    Subject: API key is added

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-
+var api_key = '750c257ad74cd91d9a1d54e0c739b736';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
