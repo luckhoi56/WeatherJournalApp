b08a03c4005830bd7edf480410cd1168e181038e
Eric Luc <ericluc@Erics-MacBook-Pro.local>
2020-03-01 00:39:11 -0500

    Subject: the server post and get from user worked

--- server.js
+++ server.js
@@ -1,19 +1,42 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = {data:'dummy'};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+
+//* Dependencies */
+/* Middleware*/
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
 
-// Cors for cross origin allowance
-
-// Initialize the main project folder
+/*Initialize the main project folder */
 app.use(express.static('website'));
 
+const port = 3000;
+/*Spin up the servers */
+const server = app.listen(port,()=>{
+    console.log(`running on local host: ${port}`);
+});
 
-// Setup Server
+//GET route
+app.get('/all',(request,response)=>{
+    response.send(projectData);
+});
+
+//POST route to check if post worked
+app.post('/add',(request,response)=>{
+    response.send('POST received ');
+});
+
+//POST data
+app.post('/user', (request,response)=>{
+    projectData.push(request.body);
+});
+
