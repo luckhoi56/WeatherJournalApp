c2941f1c0a7af85eac942465fec23edbf7d41a02
Eric Luc <ericluc@Erics-MacBook-Pro.local>
2020-03-01 20:59:35 -0500

    Subject: POST finished

--- website/app.js
+++ website/app.js
@@ -1,9 +1,9 @@
+// hard_code ="http://api.openweathermap.org/data/2.5/weather?q=91763&appid=750c257ad74cd91d9a1d54e0c739b736"
 /* Global Variables */
 var api_key = '750c257ad74cd91d9a1d54e0c739b736';
 var sent_data ={tem:'',userInput:'' ,current_date:''};
 //const fetch = require('express');
 var baseURL;
-var hardCode ='http://api.openweathermap.org/data/2.5/weather?q=91763&appid=750c257ad74cd91d9a1d54e0c739b736'
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
@@ -16,10 +16,17 @@ function performAction(e){
   const user_input_comment = document.getElementById('feelings').value;
   console.log("user zip is " + user_input_zip +"\n");
   baseURL =  `http://api.openweathermap.org/data/2.5/weather?q=${user_input_zip}&appid=${api_key}`;
-  console.log('baseURL is ' + baseURL);
-  getInformationFromAPI(baseURL, user_input_comment);
+  
+  getInformationFromAPI(baseURL)
   //send the information back to the server use POST
-  postData("http://localhost:3000/user",{tem:'100',userInput:user_input_comment ,current_date:'d'});
+  .then((data) =>{
+    console.log(`temperature data is ${data}`);
+    postData("http://localhost:3000/user",{temp:data,userInput:user_input_comment ,current_date:newDate});
+  })
+  .then(
+    updateUI()
+  )
+  
   //make a GET request to get recent entry
 }
 /// Async POST
@@ -31,12 +38,12 @@ const postData = async ( url = '', data = {})=>{
     headers: {
         'Content-Type': 'application/json',
     },
+    
     body: JSON.stringify(data), // body data type must match "Content-Type" header        
   });
 
     try {
       const newData = await response.json();
-      console.log(newData[0].tem);
       return newData;
     }catch(error) {
     console.log("error is ", error);
@@ -56,16 +63,21 @@ const retrieveData = async (url='') =>{
   }
 };
 //Async API GET
-const getInformationFromAPI = async(baseURL, user_input_comment)=>{
+const getInformationFromAPI = async(baseURL)=>{
   const res = await fetch(baseURL)
   try{
     const data = await res.json();
-    console.log(data);
+    console.log(data.main.temp);
+    return data.main.temp;
   }
   catch(error) {
     console.log("error", error);
     // appropriately handle the error
   }
 };
+
+function updateUI(){
+
+}
 
 
